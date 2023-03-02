868a38b77c7b69a675e00c378f6df184c8fab741
penguinxx <penguinpenguinxx@yahoo.co.jp>
2023-03-02 08:19:10 +0000

    [AC] abc286 abc286_c

---
+++ atcoder.jp/abc286/abc286_c/Main.py
@@ -0,0 +1,11 @@
+n, a, b = map(int, input().split())
+s = input()
+s += s
+ans = 1 << 60
+for i in range(n):
+    tmp = a * i
+    for j in range(n // 2):
+        if s[i + j] != s[i + n - 1 - j]:
+            tmp += b
+    ans = min(ans, tmp)
+print(ans)
